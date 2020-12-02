806b7f64a1463d91a21070f6abedbc18e3d0781e
Marta Camacho <martacamachovargas@gmail.com>
2020-12-02 22:59:31 +0100

    structure done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+    <header>
+      <div className="logo">
+          <img src="" alt="" />
+      </div>
+      <button className="nav-toggle" aria-label="toggle navigation">
+        <span className="hamburger"></span>
+      </button>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item"><a href="#home" className="nav-link">Home</a> </li>
+          <li className="nav-item"><a href="#sevices" className="nav-link">My services</a> </li>
+          <li className="nav-item"><a href="#about" className="nav-link">About me</a> </li>
+          <li className="nav-item"><a href="#work" className="nav-link">My work</a> </li>
+        </ul>
+      </nav>
+    </header>
+
+<section classname="intro" id="home">
+    <h1 className="section-title section-title-intro">
+      Hi, I am <strong>Marta Camacho</strong>
+    </h1>
+      <p className="section-subtitle section-subtitle-intro">Full Stack Developer</p>
+      <img src="" alt="picture of me" />
+</section>
+
+<section className="my-services" id="services">
+  <h2 className="section-title section-title-services"> What I do</h2>
+      <div className="services">
+        <div className="service">
+            <h3></h3>
+            <p></p>
+        </div>
+        <div className="service">
+            <h3></h3>
+            <p></p>
+        </div>
+        <div className="service">
+            <h3></h3>
+            <p></p>
+        </div>
+      </div>
+    <a href="#work" className="btn">My work</a>
+</section>
+      
+<section className="about-me" if="#about">
+    <h2 className="section-title section-title-about">Who I am</h2>
+    <p className="section-subtitle section-subtitle-about"></p>
+    <div className="about-me-body">
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+    <img src="" alt=""/>
+</section>
+
+<section className="my-work" id="work">
+<h2 clasName="section-title">My work</h2>
+<p className="section-subtitle"></p>
+
+<div className="portfolio">
+  <a href="#" className="portfolio-item">
+    <img src="" alt="" className="portfolio-img"/>
+  </a>
+</div>
+
+<div className="portfolio">
+  <a href="#" className="portfolio-item">
+    <img src="" alt="" className="portfolio-img"/>
+  </a>
+</div>
+
+<div className="portfolio">
+  <a href="#" className="portfolio-item">
+    <img src="" alt="" className="portfolio-img"/>
+  </a>
+</div>
+</section>
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
---
+++ src/components/Service.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react'
+
+class Service extends Component {
+    render() {
+        return (
+            <div className="service">
+                <h3></h3>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default Service;
